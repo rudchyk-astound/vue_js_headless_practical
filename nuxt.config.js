1e8250c0a699517e3cc72d1157e900243098ae1d
S.Rudchyk (Astoundcommerce) <s.rudchyk@astoundcommerce.com>
2020-11-10 15:19:57 +0200

    changed nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,28 @@
 export default {
+  mode: 'universal',
+  server: {
+    port: 9393
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'vue_js_headless_practical',
+    titleTemplate: '%s',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#39b982' },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
